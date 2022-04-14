9f4e0289c62db29fb9922410393b10df63bb0302
ANSH GUPTA <65327006+Ansh-create@users.noreply.github.com>
2022-04-14 23:08:41 +0530

    Create Move all the negative elements to one side of the array.cpp

---
+++ ARRAY/Move all the negative elements to one side of the array.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    
+    int arr[num];
+    for(int i=0; i<num; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    for(int i=0; i<num; i++)
+    {
+        cout<<arr[i];
+    }
+    cout<<"\n";
+    sort(arr, arr+num);
+    
+    for(int i=0; i<num; i++)
+    {
+        cout<<arr[i];
+    }
+    
+}
