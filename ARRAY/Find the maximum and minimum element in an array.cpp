8133974b35181e3551823dfa494a6624cf8aa2ea
ANSH GUPTA <65327006+Ansh-create@users.noreply.github.com>
2022-04-14 23:08:07 +0530

    Create Find the maximum and minimum element in an array.cpp

---
+++ ARRAY/Find the maximum and minimum element in an array.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    
+    int arr[num];
+    for(int i=0; i<num; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    for(int i=0; i<num; i++)
+    {
+        cout<<arr[i];
+    }
+    
+    sort(arr, arr+num);
+    
+    for(int i=0; i<num; i++)
+    {
+        cout<<arr[i];
+    }
+    
+    
+}
