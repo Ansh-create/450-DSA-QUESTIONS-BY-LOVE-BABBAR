666320d83619dcca5c091280929b5f22cb86d8c0
ANSH GUPTA <65327006+Ansh-create@users.noreply.github.com>
2022-04-16 22:58:23 +0530

    Create left shift & right shift.cpp

---
+++ BASICS/left shift & right shift.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=4;
+    int b=5;
+    
+    cout<<(19<<1)<<endl;
+    cout<<(20<<2)<<endl;
+    cout<<(15>>1)<<endl;
+    cout<<(20>>2)<<endl;
+}
