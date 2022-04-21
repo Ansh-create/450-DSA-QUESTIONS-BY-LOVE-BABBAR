9ff1215c4f66537fbee785d347d772f0890339c8
ANSH GUPTA <65327006+Ansh-create@users.noreply.github.com>
2022-04-21 23:26:01 +0530

    Update Air fare detection.cpp

--- BASICS/Air fare detection.cpp
+++ BASICS/Air fare detection.cpp
@@ -1,1 +1,19 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  char day;
+  cin>>day>>endl;
+  if(day==monday || day==wednesday || day== friday){
+    
+    cout<<'5000';
+  }
+  else if (day=tuesday || day==thursday){
+    cout<<'4000';
+  }
+  else{
+    cout<<'10000';
+  }
+  return 0;
+}
+    
